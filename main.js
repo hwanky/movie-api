86e0cede0a4cd9f45a2f69bf29b364da82d9e122
hwanky <choijh0925@gmail.com>
2022-11-15 21:25:15 +0900

    feat: 최대 페이지 도달 시 더보기 버튼 삭제 기능 추가

--- main.js
+++ main.js
@@ -7,8 +7,10 @@
   const searchTypeEl = document.querySelector(".search-type");
   const searchNumEl = document.querySelector(".search-num");
   const searchYearEl = document.querySelector(".search-year");
+  const bodyEl = document.querySelector("body");
 
   let page = 1;
+  let maxPage = -1;
 
   // 검색 버튼 클릭
   searchBtnEl.addEventListener("click", async (event) => {
@@ -22,7 +24,6 @@
     if (title) {
       const movies = await getMovies(title, page, type, year);
       for (let i = 0; i < num; i++) {
-        console.log("페이지+1");
         addPage();
       }
       renderMovies(movies);
@@ -37,22 +38,29 @@
     addPage();
   });
 
+  // 영화 상세정보 클릭
+
   // 영화 리스트 페이지 추가 함수
   async function addPage() {
     let title = searchTxtEl.value;
     let type = searchTypeEl.value;
     let year = searchYearEl.value;
-    const movies = await getMovies(title, page, type, year);
     page += 1;
+    const movies = await getMovies(title, page, type, year);
     renderMovies(movies);
   }
 
-  // api 호출 함수
+  // 영화 리스트 api 호출 함수
   async function getMovies(title = "", page = 1, type = "", year = "") {
     const res = await fetch(
       `https://omdbapi.com/?apikey=7035c60c&s=${title}&page=${page}&type=${type}&y=${year}`
     );
     const { Search: movies, totalResults } = await res.json();
+    maxPage = Math.ceil(Number(totalResults) / 10);
+
+    if (page >= maxPage) {
+      moreBtnEl.style.display = "none";
+    }
     return movies;
   }
 
@@ -84,6 +92,21 @@
       moviesEl.append(el);
     }
   }
+
+  // 영화 상세정보 api 호출 함수
+  async function getMovieDetail(id) {
+    const res = await fetch(
+      `https://omdbapi.com/?apikey=7035c60c&i=${id}&plot=full`
+    );
+    const movieDetail = await res.json();
+    if (movieDetail.Response === "True") {
+      return movieDetail;
+    }
+    return movieDetail.Error;
+  }
+
+  // 영화 상세정보 렌더링 함수
+  async function renderMovieDetail() {}
 
   // 영화 리스트 초기화 함수
   function initMovies() {
